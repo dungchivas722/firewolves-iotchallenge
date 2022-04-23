1a511ab790e50017696e97fdfb4cd43d63431692
Quang <98quangdz@gmail.com>
2022-04-23 14:22:40 +0700

    RK520-01

---
+++ offline/RK520-01/GNU ARM v10.2.1 - Debug/app.d
@@ -0,0 +1,3 @@
+app.o: ../app.c \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
---
+++ offline/RK520-01/GNU ARM v10.2.1 - Debug/gecko_sdk_4.0.2/platform/service/sleeptimer/src/sl_sleeptimer_hal_burtc.d
@@ -0,0 +1,148 @@
+gecko_sdk_4.0.2/platform/service/sleeptimer/src/sl_sleeptimer_hal_burtc.o: \
+ C\:/Users/98qua/SimplicityStudio/SDKs/gecko_sdk/platform/service/sleeptimer/src/sl_sleeptimer_hal_burtc.c \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\service\sleeptimer\inc\sl_sleeptimer.h \
+ C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\config/sl_sleeptimer_config.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\em_device.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm220pc22hna.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\core_cm33.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_version.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_compiler.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_gcc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\mpu_armv8.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\system_bgm22.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_emu.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_cmu.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_hfxo.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_hfrco.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_fsrco.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dpll.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_lfxo.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_lfrco.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ulfrco.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_msc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_icache.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_prs.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpio.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpio_port.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldma.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldmaxbar.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_timer.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_usart.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_burtc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_i2c.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_syscfg.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_buram.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpcrc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dcdc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_pdm.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rfsense.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_aes.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_smu.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rtcc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_letimer.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_iadc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_wdog.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_amuxcp.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_eusart.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_cryptoacc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_frc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_agc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rfcrc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_modem.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_synth.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_protimer.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rac.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_mailbox.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_bufc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_devinfo.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_prs_signals.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dma_descriptor.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldmaxbar_defines.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_status.h \
+ C\:/Users/98qua/SimplicityStudio/SDKs/gecko_sdk/platform/service/sleeptimer/src/sli_sleeptimer_hal.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\service\sleeptimer\inc\sli_sleeptimer.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_burtc.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_assert.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_bus.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_core.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_common.h \
+ C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\autogen/sl_component_catalog.h \
+ C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\config/emlib_core_debug_config.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_core.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_cmu.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_cmu_compat.h \
+ c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_gpio.h
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\service\sleeptimer\inc\sl_sleeptimer.h:
+C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\config/sl_sleeptimer_config.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\em_device.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm220pc22hna.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\core_cm33.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_version.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_compiler.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\cmsis_gcc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\cmsis\include\mpu_armv8.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\system_bgm22.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_emu.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_cmu.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_hfxo.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_hfrco.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_fsrco.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dpll.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_lfxo.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_lfrco.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ulfrco.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_msc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_icache.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_prs.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpio.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpio_port.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldma.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldmaxbar.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_timer.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_usart.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_burtc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_i2c.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_syscfg.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_buram.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_gpcrc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dcdc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_pdm.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rfsense.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_aes.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_smu.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rtcc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_letimer.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_iadc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_wdog.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_amuxcp.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_eusart.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_cryptoacc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_frc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_agc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rfcrc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_modem.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_synth.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_protimer.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_rac.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_mailbox.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_bufc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_devinfo.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_prs_signals.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_dma_descriptor.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\device\siliconlabs\bgm22\include\bgm22_ldmaxbar_defines.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\common\inc\sl_status.h:
+C\:/Users/98qua/SimplicityStudio/SDKs/gecko_sdk/platform/service/sleeptimer/src/sli_sleeptimer_hal.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\service\sleeptimer\inc\sli_sleeptimer.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_burtc.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_assert.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_bus.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_core.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_common.h:
+C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\autogen/sl_component_catalog.h:
+C\:\Users\98qua\SimplicityStudio\v5_workspace\empty\config/emlib_core_debug_config.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_core.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_cmu.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_cmu_compat.h:
+c\:\users\98qua\simplicitystudio\sdks\gecko_sdk\platform\emlib\inc\em_gpio.h:
