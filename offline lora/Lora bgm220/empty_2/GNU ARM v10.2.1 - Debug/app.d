c61b5ffb43df180e53e104c3b8fb5ae9dc26a7e4
Dun <61546385+dungchivas722@users.noreply.github.com>
2022-05-02 22:05:12 +0700

    Delete offline lora directory

--- offline lora/Lora bgm220/empty_2/GNU ARM v10.2.1 - Debug/app.d
+++
@@ -1,3 +0,0 @@
-app.o: ../app.c \
- c\:\users\dungc\simplicitystudio\sdks\gecko_sdk1\platform\common\toolchain\inc\sl_gcc_preinclude.h
-c\:\users\dungc\simplicitystudio\sdks\gecko_sdk1\platform\common\toolchain\inc\sl_gcc_preinclude.h:
